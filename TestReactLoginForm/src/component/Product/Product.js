8d0a3a187e242065d9516b7475a01af3c13e0f14
Nguyễn Hoàng Tuấn Anh <51041036+tuananhnguyenhoang@users.noreply.github.com>
2022-12-12 07:28:15 +0700

    Update Product.js

--- TestReactLoginForm/src/component/Product/Product.js
+++ TestReactLoginForm/src/component/Product/Product.js
@@ -12,28 +12,30 @@ export default function Product() {
             })
     }, [])
     return (
-        <div>
+        <div className='mg-auto container-fluid' >
             {
                 store.Name && store.Pass &&
                 store.Name.lsNameLogin.length > 0 && store.Pass.lsPassLogin.length > 0 &&
                 <p>Đăng nhập thành công</p>
             }
-            {store.MoviePr.lsGetMovie &&
-                store.MoviePr.lsGetMovie.map((e, i) => {
-                    return (
+            <div className='d-flex flex-wrap justify-content-center'>
 
-                        <div className="card fl-Left mg text-al" style={{ width: '22.5rem' }}>
-                            <img src={e.Background} className="card-img-top" alt="..." />
-                            <div className="card-body">
+                {store.MoviePr.lsGetMovie &&
+                    store.MoviePr.lsGetMovie.map((e, i) => {
+                        return (
+                            <div className="card fl-Left pa mg text-al " style={{ width: '22rem' }}>
+                                <img src={e.Background} className="card-img-top" alt="..." />
+                                <div className="card-body">
 
-                                <img width={'22%'} src={e.Logo} />
-                                <p className="card-text">{e.Slogan}</p>
-                                <a href="#" className="btn btn-primary">Go cinema</a>
+                                    <img width={'22%'} src={e.Logo} />
+                                    <p className="card-text">{e.Slogan}</p>
+                                    <a href="#" className="btn btn-primary">Go cinema</a>
+                                </div>
                             </div>
-                        </div>
-                    )
-                })
-            }
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
